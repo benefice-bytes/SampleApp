d8f26027c32985585f45ce9a28379407389d070a
Nazeeruddin <naseers.tech@gmail.com>
2021-08-23 08:50:47 +0530

    Employee class moved to Employee.cs from Program.cs

--- Program.cs
+++ Program.cs
@@ -2,18 +2,6 @@
 
 namespace SampleApp
 {
-    class Employee
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public double Salary { get; set; }
-
-        public override string ToString()
-        {
-            return $"{Id} - {Name} - {Salary}";
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
